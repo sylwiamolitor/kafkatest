3dc0fc6816c85fa435713c6e7673854e8fe4c82a
Sylwia <sylwiamlr@gmail.com>
2024-03-05 21:27:53 +0100

    Added OpenAPI documentation

--- src/main/java/com/sylwia/kafkatest/MessageController.java
+++ src/main/java/com/sylwia/kafkatest/MessageController.java
@@ -1,5 +1,7 @@
 package com.sylwia.kafkatest;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -8,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api/v1/messages")
+@Tag(name = "Operations on messages.")
 public class MessageController {
     private KafkaTemplate<String, String> kafkaTemplate;
 
@@ -16,6 +19,7 @@ public class MessageController {
     }
 
     @PostMapping
+    @Operation(summary = "Method for publishing messages.")
     public void publish(@RequestBody MessageRequest request) {
         kafkaTemplate.send("sylwia", request.message());
     }
